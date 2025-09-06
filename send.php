5d92e41d275bf9e9b9116ec71b5a0ab6d7464e2c
Maximus261 <temita345@gmail.com>
2025-09-06 21:20:42 +0100

    Add files via upload

---
+++ send.php
@@ -0,0 +1,42 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Load PHPMailer
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'temita345@gmail.com';         // Gmail
+        $mail->Password   = 'ixqc kvjl tpum kkuz';            // App password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+        // Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('temita345@gmail.com');         // Where the message goes
+
+        // Content
+        $mail->isHTML(false);
+        $mail->Subject = 'New Message';
+        $mail->Body    = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+        $mail->send();
+        echo 'Message has been sent successfully.';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
